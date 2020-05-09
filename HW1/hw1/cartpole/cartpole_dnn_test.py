fce6ac7078598a81dfd583a1bfc679a5344c726f
avk4714 <avkalia@uw.edu>
2020-05-09 16:05:44 -0700

    ReLU training works better than TanH

--- HW1/hw1/cartpole/cartpole_dnn_test.py
+++ HW1/hw1/cartpole/cartpole_dnn_test.py
@@ -10,14 +10,14 @@ rng = np.random.RandomState(54454) #12345
 # State representation
 # dtheta, dx, theta, x
 
-kernel_length_scales = np.array([[240.507, 242.9594, 218.0256, 203.0197],
-                                 [175.9314, 176.8396, 178.0185, 33.0219],
-                                 [7.4687, 7.3903, 13.0914, 34.6307],
-                                 [0.8433, 1.0499, 1.2963, 2.3903],
-                                 [0.781, 0.9858, 1.7216, 31.2894],
-                                 [23.1603, 24.6355, 49.9782, 219.185]])
-kernel_scale_factors = np.array([3.5236, 1.3658, 0.7204, 1.1478])
-noise_sigmas = np.array([0.0431, 0.0165, 0.0145, 0.0143])
+# kernel_length_scales = np.array([[240.507, 242.9594, 218.0256, 203.0197],
+#                                  [175.9314, 176.8396, 178.0185, 33.0219],
+#                                  [7.4687, 7.3903, 13.0914, 34.6307],
+#                                  [0.8433, 1.0499, 1.2963, 2.3903],
+#                                  [0.781, 0.9858, 1.7216, 31.2894],
+#                                  [23.1603, 24.6355, 49.9782, 219.185]])
+# kernel_scale_factors = np.array([3.5236, 1.3658, 0.7204, 1.1478])
+# noise_sigmas = np.array([0.0431, 0.0165, 0.0145, 0.0143])
 
 
 def sim_rollout(sim, policy, n_steps, dt, init_state):
@@ -53,8 +53,13 @@ def augmented_state(state, action):
     :param action: action applied to state
     :return: an augmented state for training GP dynamics
     """
-    dtheta, dx, theta, x = state
-    return x, dx, dtheta, np.sin(theta), np.cos(theta), action
+    dtheta = state[0]
+    dx = state[1]
+    theta = state[2]
+    x = state[3]
+
+
+    return np.array([x, dx, dtheta, np.sin(theta), np.cos(theta), action])
 
 
 def make_training_data(state_traj, action_traj, delta_state_traj):
@@ -100,7 +105,7 @@ if __name__ == '__main__':
     d_in = 6            # Inputs are: [p, dp, dtheta, sin(theta), cos(theta), action]
     d_out = 4           # Outputs are: [ddtheta, ddp, dtheta, dp]
     model = Net(d_in, n_hidden, d_out, z_prob, lam_mult)
-    PATH = 'cartpole_TanH.pth'
+    PATH = 'cartpole_ReLU.pth'
     model.load_state_dict(torch.load(PATH))
     model.eval()
 
@@ -119,50 +124,68 @@ if __name__ == '__main__':
         delta_state_traj = state_traj[1:] - state_traj[:-1]
 
         # DNN Rollout
-        pred_state_traj = []
+        
         for j in range(NUM_DATAPOINTS_PER_EPOCH):
             if j == 0:
-                pred_state = init_state
+                pred_state_traj = init_state.reshape(4,1)
+                pred_state = init_state.reshape(4,1)
+            else:
+            	# import pdb; pdb.set_trace()
+            	pred_state_traj = np.append(pred_state_traj,pred_state.reshape(4,1),axis=1)
 
-            pred_state_traj.append(pred_state,axis=0)
-            aug_state = augmented_state(pred_state_traj[j], action_traj[j])
+            # import pdb; pdb.set_trace()
+            aug_state = augmented_state(pred_state_traj[:,j], action_traj[j])
+            # import pdb; pdb.set_trace()
             aug_state_tnsr = Variable(torch.from_numpy(aug_state))
+            # import pdb; pdb.set_trace()
 
             # Run through the model
-            pred_state_delta = model(aug_state_tnsr).numpy()
+            pred_state_delta = model(aug_state_tnsr.float()).detach().numpy()
+            pred_state_delta = pred_state_delta.reshape(4,1)
+            # import pdb; pdb.set_trace()
 
             # State update
-            pred_state += pred_state_delta
+            pred_state = np.add(pred_state,pred_state_delta)
 
         dnn_state_traj = np.copy(pred_state_traj)
         dnn_delta_state_traj = dnn_state_traj[1:] - dnn_state_traj[:-1]
 
-        for i in range(len(state_traj) - 1):
-            vis.set_gt_cartpole_state(state_traj[i][3], state_traj[i][2])
-            vis.set_gt_delta_state_trajectory(ts[:i+1], delta_state_traj[:i+1])
+        # Plot Training Y and Predicted Y
+        t_vec = np.arange(0.0,(NUM_DATAPOINTS_PER_EPOCH + 1) * DELTA_T,DELTA_T)
+        # import pdb; pdb.set_trace()
+        plt.plot(t_vec,state_traj[:,2])
+        plt.plot(t_vec[:-1],dnn_state_traj.T[:,2],'--')
+        plt.grid()
+        # plt.ylim((-10,10))
+        # plt.xlim((-10,10))
+        plt.savefig("test_comparison_3.png")
+        # plt.show()
+        # for i in range(len(state_traj) - 1):
+        #     vis.set_gt_cartpole_state(state_traj[i][3], state_traj[i][2])
+        #     vis.set_gt_delta_state_trajectory(ts[:i+1], delta_state_traj[:i+1])
 
-            vis.set_gp_cartpole_state(dnn_state_traj[i][3], dnn_state_traj[i][2])
-            vis.set_gp_delta_state_trajectory(ts[:i+1], dnn_delta_state_traj[:i+1])
+        #     vis.set_gt_cartpole_state(dnn_state_traj[i][3], dnn_state_traj[i][2])
+        #     vis.set_gt_delta_state_trajectory(ts[:i+1], dnn_delta_state_traj[:i+1])
 
-            if policy == swingup_policy:
-                policy_type = 'swing up'
-            else:
-                policy_type = 'random'
+        #     if policy == swingup_policy:
+        #         policy_type = 'swing up'
+        #     else:
+        #         policy_type = 'random'
 
-            vis.set_info_text('epoch: %d\npolicy: %s' % (epoch, policy_type))
+        #     vis.set_info_text('epoch: %d\npolicy: %s' % (epoch, policy_type))
 
-            vis_img = vis.draw(redraw=(i==0))
-            cv2.imshow('vis', vis_img)
+        #     vis_img = vis.draw(redraw=(i==0))
+        #     cv2.imshow('vis', vis_img)
 
-            if epoch == 0 and i == 0:
-                # First frame
-                video_out = cv2.VideoWriter('cartpole.mp4',
-                                            cv2.VideoWriter_fourcc('m', 'p', '4', 'v'),
-                                            int(1.0 / DELTA_T),
-                                            (vis_img.shape[1], vis_img.shape[0]))
+        #     if epoch == 0 and i == 0:
+        #         # First frame
+        #         video_out = cv2.VideoWriter('cartpole.mp4',
+        #                                     cv2.VideoWriter_fourcc('m', 'p', '4', 'v'),
+        #                                     int(1.0 / DELTA_T),
+        #                                     (vis_img.shape[1], vis_img.shape[0]))
 
-            video_out.write(vis_img)
-            cv2.waitKey(int(1000 * DELTA_T))
+        #     video_out.write(vis_img)
+        #     cv2.waitKey(int(1000 * DELTA_T))
 
         # Augment training data
         # new_train_x, new_train_y = make_training_data(state_traj[:-1], action_traj, delta_state_traj)
